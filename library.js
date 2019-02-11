bfe99830c8f60a58aef480d697e8e0e99cdb2bdb
Adrien Rocher Vj <a.rocher.vj@gmal.com>
2019-02-11 18:27:15 +0100

    Migrate to passport-ldapauth

--- library.js
+++ library.js
@@ -1,15 +1,19 @@
 (function (module) {
     "use strict";
 
-    var User = module.parent.require('./user'),
-        meta = module.parent.require('./meta'),
-        db = module.parent.require('../src/database'),
-        passport = module.parent.require('passport'),
-        passportLdap = require('passport-ldap').Strategy,
-        fs = module.parent.require('fs'),
-        path = module.parent.require('path'),
-        nconf = module.parent.require('nconf');
+    var User = require.main.require("./src/user"),
+        meta = require.main.require("./src/meta"),
+        db = require.main.require("./src/database"),
+	      passport = require.main.require('passport'),
+        passportLdap = require('passport-ldapauth').Strategy,
+        fs = require.main.require('fs'),
+        path = require.main.require('path'),
+        nconf = require.main.require('nconf'),
+        async = require.main.require('async'),
+        winston = require.main.require('winston');
 
+    var authenticationController = require.main.require('./src/controllers/authentication');
+    
     var constants = Object.freeze({
         'name': "LDAP Account",
         'admin': {
@@ -20,48 +24,67 @@
 
     var Ldap = {};
 
-    Ldap.init = function (app, middleware, controllers) {
+    Ldap.init = function (params, callback) {
         function render(req, res, next) {
             res.render('admin/plugins/sso-ldap', {});
         }
+        console.log("sso-ldap starting");
+        params.router.get('/admin/plugins/sso-ldap', params.middleware.admin.buildHeader, render);
+        params.router.get('/api/admin/plugins/sso-ldap', render);
 
-        app.get('/admin/plugins/sso-ldap', middleware.admin.buildHeader, render);
-        app.get('/api/admin/plugins/sso-ldap', render);
+        callback();
     };
 
     Ldap.getStrategy = function (strategies, callback) {
         meta.settings.get('sso-ldap', function (err, settings) {
+            //console.log("sso-ldap getStrategy: ", (!err && settings['server'] && settings['username'] && settings['secret'] && settings['base'] && settings['filter'] && settings['attributes'] && settings['searchAttributes']));
+            console.log("sso-ldap getStrategy: ", settings['searchAttributes'] );
             if (!err && settings['server'] && settings['username'] && settings['secret'] && settings['base'] && settings['filter'] && settings['attributes'] && settings['searchAttributes']) {
+                console.log("print debug", settings['server']);
                 passport.use(new passportLdap({
                     server: {
                         url: settings['server']
                     },
                     usernameField: settings['username'],
                     passwordField: settings['secret'],
-                    base: (settings['base']).split(','),
-                    search: {
+                    searchBase: (settings['base']).split(','),
+                    /*search: {
                         filter: settings['filter'],
                         scope: 'sub',
                         attributes: (settings['attributes']).split(','),
                         sizeLimit: 1
-                    },
+                    },*/
+                    searchFilter: settings['filter'],
                     searchAttributes: settings['searchAttributes']
 
                 }, function (accessToken, refreshToken, profile, callback) {
                     Ldap.login(profile.id, profile.displayName, profile.emails[0].value, function (err, user) {
                         if (err) {
+                            console.log("sso-ldap login error", err, new Error('sso-ldap').stack);
                             return callback(err);
+                        } else {
+                            console.log("sso-ldap login", user);
                         }
-                        return callback(null, user);
+                        //return callback(null, user);
+                        authenticationController.onSuccessfulLogin(req, user.uid, function (err) {
+                          done(err, !err ? user : null);
+                        });
                     });
                 }));
+
+                console.log("sso-ldap strategies pushing");
 
                 strategies.push({
                     name: 'ldap',
                     url: '/auth/ldap',
                     callbackURL: '/auth/ldap/callback',
                     icon: 'fa-user',
+                    scope: 'public_profile, email'
                 });
+                console.log("sso-ldap strategies pushed");
+            }
+            else {
+                console.log("root error ", settings);
             }
 
             callback(null, strategies);
